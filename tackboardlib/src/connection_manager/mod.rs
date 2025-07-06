38bd2e493cb66f2144ce40bf9f8a7d7555a12037
AlexsJones <alexsimonjones@gmail.com>
2025-07-06 17:25:18 +0100

    feat: implemented first part of the client server handshake
    Signed-off-by: AlexsJones <alexsimonjones@gmail.com>

--- tackboardlib/src/connection_manager/mod.rs
+++ tackboardlib/src/connection_manager/mod.rs
@@ -1,19 +1,24 @@
+use std::collections::HashMap;
+use std::sync::Arc;
 use tokio_util::codec::Framed;
 use tokio_util::codec::LengthDelimitedCodec;
 use tokio_serde::formats::*;
 use futures::prelude::*;
+use log::error;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
 use crate::types::{ClientRequest, ServerResponse, Topic};
 use crate::types::errors::TackboardError;
 pub trait Connects {
 
     //Client side
-    fn connect(&self, request: ClientRequest) -> Result<ServerResponse, TackboardError>;
+    async fn connect(&self, request: ClientRequest) -> Result<ServerResponse, TackboardError>;
     fn subscribe(&self, request: ClientRequest) -> Result<ServerResponse, TackboardError>;
     fn publish(&self, request: ClientRequest) -> Result<ServerResponse, TackboardError>;
 
     // Server side
-    async fn accept_connection<F, Fut>(&self,callback: F) where F: Fn(String) -> Fut + Send + 'static,
+    async fn accept_connection<F, Fut>(&mut self,callback: F) -> Result<(), TackboardError>
+    where F: Fn(ClientRequest, &InConnection) -> Fut + Send + 'static,
         Fut: Future<Output=()> + Send + 'static;
 }
 
@@ -23,31 +28,46 @@ ClientRequest, Json<ServerResponse, ClientRequest>>;
 
 type InConnection = tokio_serde::Framed<Framed<tokio::net::TcpStream, LengthDelimitedCodec>,
     ClientRequest,
-    ServerResponse, Json<ClientRequest, ClientRequest>>;
+    ServerResponse, Json<ClientRequest, ServerResponse>>;
+fn create_incoming_connection(
 
+    length_delimited: Framed<TcpStream, LengthDelimitedCodec>,
+) -> InConnection {
+    tokio_serde::Framed::new(
+        length_delimited,
+        Json::<ClientRequest, ServerResponse>::default(),
+    )
+}
+fn create_outgoing_connection(
+    length_delimited: Framed<TcpStream, LengthDelimitedCodec>,
+) -> OutConnection {
+    tokio_serde::Framed::new(
+        length_delimited,
+        Json::<ServerResponse, ClientRequest>::default(),
+    )
+}
 #[derive(Default)]
 pub struct ConnectionManager {
     path: String,
     listener: Option<TcpListener>,
     client_urls: Vec<String>,
-    topic_listens: Vec<(Topic, String)>,
-
+    topics: Arc<Mutex<HashMap<String, Vec<String>>>>
 }
 
 impl ConnectionManager {
-    pub async fn create_listener(path: String) -> Result<ConnectionManager, TackboardError> {
+    pub async fn create_listener(path: String, topics: Arc<Mutex<HashMap<String, Vec<String>>>>) -> Result<ConnectionManager, TackboardError> {
         let listener = TcpListener::bind(&path).await.map_err(TackboardError::Io)?;
         Ok(ConnectionManager {
             path,
             listener: Some(listener),
             client_urls: vec![],
-            topic_listens: vec![],
+            topics,
         })
     }
 }
 
 impl Connects for ConnectionManager {
-    fn connect(&self, request: ClientRequest) -> Result<ServerResponse, TackboardError> {
+    async fn connect(&self, request: ClientRequest) -> Result<ServerResponse, TackboardError> {
         todo!()
     }
 
@@ -58,13 +78,38 @@ impl Connects for ConnectionManager {
     fn publish(&self, request: ClientRequest) -> Result<ServerResponse, TackboardError> {
         todo!()
     }
-    
 
-    async fn accept_connection<F, Fut>(&self, callback: F)
+
+    async fn accept_connection<F, Fut>(&mut self, callback: F) -> Result<(), TackboardError>
     where
-        F: Fn(String) -> Fut + Send + 'static,
+        F: Fn(ClientRequest, &InConnection) -> Fut + Send + 'static,
         Fut: Future<Output=()> + Send + 'static
     {
-        todo!()
+        let (socket, _) = self.listener
+            .as_mut()
+            .unwrap()
+            .accept()
+            .await?;
+        
+        tokio::spawn(
+            async move {
+                let length_delimited = Framed::new(socket, LengthDelimitedCodec::new());
+                let mut framed: InConnection = create_incoming_connection(length_delimited);
+                while let Some(message) = framed.next().await {
+                    match message {
+                        Ok(message) => {
+                            callback(message, &framed).await;
+                            // framed.send(AddMessageResponse {
+                            //     // Callback from the server to the client
+                            //     message: "OK".to_string()
+                            // }).await.expect("something went wrong!");
+                        }
+                        Err(e) => {
+                            error!("{e}");
+                        }
+                    }
+                }
+            });
+        Ok(())
     }
 }
--- tackboardsrv/src/main.rs
+++ tackboardsrv/src/main.rs
@@ -1,29 +1,63 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::sync::Arc;
+use futures::SinkExt;
 use log::debug;
+use tokio::sync::Mutex;
 use tackboardlib::connection_manager::{ConnectionManager, Connects};
+use tackboardlib::types::*;
+use once_cell::sync::Lazy;
+
+static TOPICS: Lazy<Arc<Mutex<HashMap<String, Vec<String>>>>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    for id in 0..5 {
+        let topic = format!("topic-{}", id);
+        map.insert(topic, vec![]); // No clients yet
+    }
+    Arc::new(Mutex::new(map))
+});
 
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
 
     //  Start some random topics
+    let topics = TOPICS.clone();
+    let mut connection_manager = ConnectionManager::create_listener("127.0.0.1:5621".to_string(), topics).await.unwrap();
+    // connected clients tracker
+    let mut connected_clients: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
     
 
-    let mut connection_manager = ConnectionManager::create_listener("127.0.0.1:5621".to_string()).await.unwrap();
-
- 
     tokio::spawn(async move {
         // Process jobs
         loop {
-         
+
         }
-    }); 
+    });
 
     loop {
-    
-        connection_manager.accept_connection(move |x| {
-          
+        let connected_clients = connected_clients.clone();
+        let _ = connection_manager.accept_connection(move |x, framed| {
+            let connected_clients = connected_clients.clone();
             async move {
-               
+                match x {
+                    ClientRequest::ConnectionRequest { id, client_url } => {
+
+                        debug!("Client connection request: {:?}", id);
+                        connected_clients.lock().await.push(id);
+                        let topic_keys: Vec<String> = TOPICS
+                            .lock()
+                            .await
+                            .keys()
+                            .cloned()
+                            .collect();
+                        let response = ServerResponse::ConnectionResponse {
+                            topics: topic_keys,
+                        };
+                    }
+                    ClientRequest::TopicListenRequest { .. } => {}
+                    ClientRequest::PublishRequest { .. } => {}
+                }
             }}).await;
 
     }
